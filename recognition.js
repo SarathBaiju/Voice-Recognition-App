838da3b246ac49066291e1df8b1b9a83a1fb238a
Sarath baiju <sarath.bu@pitsolutions.com>
2020-11-23 15:58:19 +0530

    Initial checkin

---
+++ recognition.js
@@ -0,0 +1,29 @@
+(function(){
+    
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
+     
+    var actionDiv = document.getElementById("action");
+    var output = document.getElementById("result");
+
+    //speech recognition events 
+    recognition.onstart = function(){
+        actionDiv.innerHTML="startSpeakText"
+    };
+
+    recognition.onspeechend = function(){
+        action.innerHTML = "<small>stopped listening, hope you are done...</small>";
+        recognition.stop();
+    };
+
+    recognition.onresult = function(event){
+        var transcript = event.results[0][0].transcript;
+        var confidence = event.results[0][0].confidence;
+         output.innerHTML = "<b>Text:</b> " + transcript + "<br/> <b>Confidence:</b> " + confidence*100+"%";
+    };
+
+    document.getElementById("startSpeakBtn").addEventListener("click", function(){
+        recognition.start();
+    });
+
+})();
